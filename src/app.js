e62609c79bd8fc7abbca7886f83da847e36dfdf1
Tek Loon <tekloon.1991@gmail.com>
2021-08-15 14:56:46 +0800

    Add SwaggerJSDoc and Swagger UI for API Documentation

--- src/app.js
+++ src/app.js
@@ -6,9 +6,116 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+
+/** 
+ * @swagger
+ * components:
+ *   schemas:
+ *     Ride:
+ *       type: object
+ *       properties:
+ *         rideID:
+ *           type: integer
+ *           description: auto-generated ride id from db
+ *         startLat:
+ *           type: number
+ *           description: Latitude of starting position
+ *         startLong:
+ *           type: number
+ *           description: Longitude of starting position
+ *         endLat:
+ *           type: number
+ *           description: Latitude of ending position
+ *         endLong:
+ *           type: number
+ *           description: Longitude of ending position
+ *         riderName:
+ *           type: string
+ *           description: Rider Name
+ *         driverName:
+ *           type: string
+ *           description: Driver Name
+ *         driverVehicle:
+ *           type: string
+ *           description: Vehicle No of the Driver
+ *         created:
+ *           type: string
+ *           description: Created date time in ISO format
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         error_code:
+ *           type: string
+ *           description: Error type
+ *         message:
+ *           type: string
+ *           description: Details of the error
+*/
+
 module.exports = (db) => {
+    /**
+     * @swagger
+     * /health:
+     *   get:
+     *     description: Health Check API
+     *     responses:
+     *       200:
+     *         description: Returns string of 'Healthy' to inform the Backend Application is in healthy condition
+     *         content:
+     *           text/plain:
+     *             schema:
+     *               type: string
+     */
     app.get('/health', (req, res) => res.send('Healthy'));
 
+    /**
+     * @swagger
+     * /rides/:
+     *   post:
+     *     summary: Create a new ride
+     *     requestBody:
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               start_lat:
+     *                 type: number
+     *                 description: Latitude of starting position
+     *               start_long:
+     *                 type: number
+     *                 description: Longitude of starting position
+     *               end_lat:
+     *                 type: number
+     *                 description: Latitude of ending position
+     *               end_long:
+     *                 type: number
+     *                 description: Longitude of ending position
+     *               rider_name:
+     *                 type: string
+     *                 description: Rider name
+     *               driver_name:
+     *                 type: string
+     *                 description: Driver name
+     *               driver_vehicle:
+     *                 type: string
+     *                 description: Driver vehicle number      
+     *     responses:
+     *       200:
+     *        description: Return created ride object
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              $ref: '#/components/schemas/Ride'
+     *       Error:
+     *         description: Error response structure
+     *         content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              $ref: '#/components/schemas/ErrorResponse'
+     */
     app.post('/rides', jsonParser, (req, res) => {
         const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
@@ -76,6 +183,29 @@ module.exports = (db) => {
         });
     });
 
+    /**
+     * @swagger
+     * /rides/:
+     *   get:
+     *     description: Returns all rides
+     *     summary: Return all rides from DB
+     *     responses:
+     *       200:
+     *        description: Lists of rides
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: array
+     *              items:
+     *                $ref: '#/components/schemas/Ride'
+     *       Error:
+     *         description: Error response structure
+     *         content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              $ref: '#/components/schemas/ErrorResponse'
+     */
     app.get('/rides', (req, res) => {
         db.all('SELECT * FROM Rides', function (err, rows) {
             if (err) {
@@ -96,6 +226,36 @@ module.exports = (db) => {
         });
     });
 
+    /**
+     * @swagger
+     * /rides/{id}:
+     *   get:
+     *     description: Returns rides based on ID
+     *     summary: Find ride by ID
+     *     operationId: getRideById
+     *     responses:
+     *       200:
+     *        description: ride response
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              $ref: '#/components/schemas/Ride'
+     *       Error:
+     *         description: Error response structure
+     *         content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              $ref: '#/components/schemas/ErrorResponse'
+     *   parameters:
+     *   - name: id
+     *     in: path
+     *     required: true
+     *     description: the user identifier, as userId 
+     *     schema:
+     *       type: string
+     */
     app.get('/rides/:id', (req, res) => {
         db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
             if (err) {
---
+++ swaggerSpec.js
@@ -0,0 +1,28 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Backend Coding Test API Docs',
+    version: '1.0.0',
+    description: 'Simple App for Coding Test',
+    contact: {
+      name: "Tek Loon",
+      url: "https://tekloon.dev",
+      email: "tekloon.1991@gmail.com",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8010",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./src/*.js'],
+};
+
+module.exports = swaggerJSDoc(options);
